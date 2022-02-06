b75b3e6d65e308c32494e04f4adf9d2fe212108b
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-02-06 21:58:41 +0100

    create a basic structure

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+class LRUStorage {
+  private capacity: number;
+  private cache: any;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
+
+  setData(key: string, value: any) {
+    if (this.cache.has(key)) {
+      const data = this.cache.get(key);
+      this.cache.delete(key);
+      data.cache.value = value;
+      this.cache.set(key, data);
+    } else {
+      this.cache.set(key, {
+        value,
+        persistent: false
+      });
+    }
+
+    const keys = Array.from(this.cache.keys());
+
+    while (this.cache.size < this.capacity) {
+      let wasDeleted = false;
+
+      if (!this.cache.size) {
+        return false;
+      }
+
+      while (!wasDeleted && this.cache.size) {
+        const key = keys.shift();
+        const item = this.cache.get(key);
+
+        if (!item.persistent) {
+          wasDeleted = true;
+          this.cache.delete(key);
+        }
+      }
+    }
+
+    if (!this.cache.has(key)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  getData(key: string) {
+    if (!this.cache.has(key)) {
+      return;
+    }
+
+    const data = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, data);
+
+    return data;
+  }
+
+  removeData(key: string) {
+    return this.cache.delete(key);
+  }
+
+  makePersistent(key: string) {
+    if (this.cache.has(key)) {
+      this.cache.get(key).persistent = true;
+    }
+  }
+}
